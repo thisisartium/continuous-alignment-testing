ad6311559d6f182b53ea20036bdbf98fb43de357
Tim Kersey <tim@artium.ai>
2025-03-06 11:28:55 -0800

    test: add hallucination test for LLM with no data
    Signed-off-by: Paul Zabelin <paulzabelin@artium.ai>

--- examples/team_recommender/tests/example_1_text_response/test_good_fit_for_project.py
+++ examples/team_recommender/tests/example_1_text_response/test_good_fit_for_project.py
@@ -34,3 +34,35 @@ def test_response_shows_developer_names():
     print(response)
     assert "Sam Thomas" in response
     assert "Drew Anderson" in response, "Surprisingly Drew Anderson is on vacation but still in the response"
+
+def test_llm_will_hallucinate_given_no_data():
+    client = OpenAI()
+    assert client is not None
+
+    system_prompt = """
+        You will get a description of a project, and your task is to tell me the best developers from the given list for the project
+         based on their skills.
+        Today's date is April 15th, 2025.
+        Pick only developers who are available after the project start date. Pick people with higher skill levels first.
+
+        Here is the skills data:
+        """
+
+    project_description = """
+        This is a mobile iOS project for a telecom company. The project starts June 3rd.
+        It will find exciting moments from sports highlights videos. The app should only work on iPhone, not iPad.
+        The tech stack is iOS Native.
+        """
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",  # here we use a smaller model to expose the hallucination
+        # model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": project_description},
+        ],
+    )
+    response = completion.choices[0].message.content
+    print(response)
+    assert "Sam Thomas" not in response, "LLM obviously could not get our expected developer and will hallucinate"
+    assert "Drew Anderson" not in response, "Response will contain made up names"
+    assert len(response.split('\n')) > 5, "response contains list of made up developers in multiple lines"
