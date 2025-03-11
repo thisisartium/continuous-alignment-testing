403cc2034124e742ad7a3f0637d443628932985d
Paul Zabelin <paulz@users.noreply.github.com>
2025-03-10 20:22:24 -0700

    add src folder to pytest path

--- examples/team_recommender/conftest.py
+++ examples/team_recommender/conftest.py
@@ -1,4 +1,10 @@
 from dotenv import load_dotenv
+import sys
+from pathlib import Path
 
 # Load environment variables from .env file
 load_dotenv()
+
+source_folder = str((Path(__file__).parent / "src").resolve())
+print("source_folder", source_folder)
+sys.path.append(source_folder)
