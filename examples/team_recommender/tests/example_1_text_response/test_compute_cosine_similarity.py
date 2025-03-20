375b5692e146382339b0c5dc4c0189412e23469d
Carl Jackson <carl@realvr.ai>
2025-03-20 16:00:32 -0700

    - lint changes and add assertion for 900 outliers with 0.0001 tolerance

--- examples/team_recommender/tests/example_1_text_response/test_compute_cosine_similarity.py
+++ examples/team_recommender/tests/example_1_text_response/test_compute_cosine_similarity.py
@@ -12,7 +12,7 @@ def load_snapshot_value(snapshot, snapshot_filename):
     """Load a snapshot file and parse it as JSON."""
     snapshot_dir = os.path.join(
         os.path.dirname(__file__),
-        'snapshots',
+        "snapshots",
         "test_compute_cosine_similarity",
         "test_reproducing_the_same_text_embedding",
     )
@@ -20,6 +20,7 @@ def load_snapshot_value(snapshot, snapshot_filename):
 
     with open(snapshot_path, "r") as f:
         return json.loads(f.read())
+
 
 def test_compute_cosine_similarity():
     assert compute_cosine_similarity([1, 2, 3], [1, 2, 3]) == 1.0
@@ -31,23 +32,38 @@ def test_compute_cosine_similarity():
     )
     assert cosine_similarity == pytest.approx(1.0)
 
+
 def test_reproducing_the_same_text_embedding(snapshot):
     saved_response = load_json_fixture("hallucination_response.json")
-    embedding_object = create_embedding_object(saved_response["text"], model="text-embedding-3-large")
+    embedding_object = create_embedding_object(
+        saved_response["text"], model="text-embedding-3-large"
+    )
     embedding_object_string = json.dumps(embedding_object, indent=2)
-    snapshot.assert_match(embedding_object_string, "hallucination_response_large_same_text_embedding.json")
-
+    snapshot.assert_match(
+        embedding_object_string, "hallucination_response_large_same_text_embedding.json"
+    )
 
 
 def test_embedding_equivalence(snapshot):
-    snap_same = load_snapshot_value(snapshot, "hallucination_response_large_same_text_embedding.json")
-    snap_different = load_snapshot_value(snapshot, "hallucination_response_large_different_text_embedding.json")
+    snap_same = load_snapshot_value(
+        snapshot, "hallucination_response_large_same_text_embedding.json"
+    )
+    snap_different = load_snapshot_value(
+        snapshot, "hallucination_response_large_different_text_embedding.json"
+    )
     # assert snap_same == snap_different
     diff_val = np.subtract(snap_same["embedding"], snap_different["embedding"])
 
-    outside_tolerance_count = np.sum(np.abs(diff_val) >= 0.00001)
+    outside_tolerance_count = np.sum(np.abs(diff_val) >= 0.001)
 
     # Assert a specific count (replace 0 with your expected count)
     assert outside_tolerance_count == 0, (
+        f"Found {outside_tolerance_count} elements outside tolerance"
+    )
+
+    outside_tolerance_count = np.sum(np.abs(diff_val) >= 0.0001)
+
+    # Assert a specific count (replace 0 with your expected count)
+    assert outside_tolerance_count == 900, (
         f"Found {outside_tolerance_count} elements outside tolerance"
     )
