2741c08b28751cda3241262282699d0dfe904913
Carl Jackson <carl@realvr.ai>
2025-03-24 16:30:57 -0700

    refactor: improve statistical significance tests and add new cases
    Signed-off-by: Paul Zabelin <paulzabelin@artium.ai>

--- examples/team_recommender/tests/test_proportions_ztest.py
+++ examples/team_recommender/tests/test_proportions_ztest.py
@@ -1,5 +1,6 @@
 import pytest
 from statsmodels.stats.proportion import proportions_ztest
+from test_helpers import next_success_rate
 
 
 def test_proportions_ztest_improvement():
@@ -32,7 +33,7 @@ def test_proportions_ztest_significantly_better():
 def test_proportions_ztest_not_statistically_significantly():
     for count in range(4, 8):
         stat, p_value = proportions_ztest(count, 10, 0.7)
-        assert p_value > 0.05, "NOT statistically significant improvement"
+        assert p_value > 0.05, "NO statistically significant deviation"
 
 
 def test_proportions_ztest_significantly_worse():
@@ -46,45 +47,37 @@ def test_proportions_ztest_significantly_worse():
     )
 
 
-def test_proportions_ztest_no_change_is_not_significant():
-    successes = [70, 70]
-    n_observations = [100, 100]
-
-    stat, p_value = proportions_ztest(successes, n_observations)
-    assert p_value == 1
-    assert p_value > 0.05, "statistically insignificant result"
-    assert stat == 0
-
-
-def calculate_p_value(success, failure, sample_size):
+def calculate_p_value(success, failure, sample_size) -> float:
     return calculate_ztest(success, failure, sample_size)[1]
 
 
-def calculate_ztest(success, failure, sample_size):
-    measurements = [int(success * sample_size), failure]
+def calculate_ztest(success, failure, sample_size) -> tuple[float, float]:
+    measurements = [int(success * sample_size), sample_size - failure]
     samples = [sample_size, sample_size]
-    return proportions_ztest(measurements, samples)
+    zstat, p_value = proportions_ztest(measurements, samples)
+    return zstat, p_value
 
 
-def is_statistically_expected(success, failure, sample_size):
+def is_statistically_significant(success, failure, sample_size):
     return calculate_p_value(success, failure, sample_size) < 0.05
 
 
+def test_not_is_statistically_significant():
+    assert not is_statistically_significant(0.7, 3, 10), "same proportion"
+    assert not is_statistically_significant(0.9, 10, 100), "same proportion"
+    assert not is_statistically_significant(0.7, 30, 100), "same proportion"
+    assert not is_statistically_significant(0.7, 0, 10), "covers 100% success rate"
+
+
 def test_is_statistically_significant():
-    assert not is_statistically_expected(0.7, 3, 10)
-    assert not is_statistically_expected(0.7, 70, 100)
-    assert is_statistically_expected(0.9, 10, 100)
-    assert is_statistically_expected(0.7, 30, 100)
+    assert is_statistically_significant(0.9, 0, 100), "0 out of 100 > 90% success rate"
+    assert is_statistically_significant(0.7, 0, 11), "0 out of 11 > 70% success rate"
+    assert is_statistically_significant(0.9, 0, 31), "0 out of 31 > 90% success rate"
+    assert is_statistically_significant(0.909090, 0, 33), "0 out of 33 > 90.9% success rate"
 
-    assert not is_statistically_expected(0.7, int(40 * 0.7), 40)
-    print("finding")
-    for i in range(40, 80):
-        if is_statistically_expected(0.7, int(i * 0.7), i):
-            print(i)
-            break
-    assert not is_statistically_expected(0.7, 31, 45)
 
-    assert not is_statistically_expected(0.7, 3, 10)
-    assert is_statistically_expected(0.7, 2, 10)
-    assert is_statistically_expected(0.9, 1, 10)
-    assert is_statistically_expected(0.9, 10, 100)
+def test_is_statistically_significant_with_next_success_rate():
+    sample_size = 10
+    assert not is_statistically_significant(next_success_rate(sample_size), 0, sample_size)
+    assert is_statistically_significant(next_success_rate(sample_size), 0, 34)
+    assert is_statistically_significant(next_success_rate(35), 0, 109)
